a653056764b484f9f4fc7ea10ea4d9ce4f418ab3
10dy10 <kdy93102@naver.com>
2022-05-24 17:21:59 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
-  <div class="wrap">
-    <section class="back-slide">
+  <div class="wrap" data-scroll-container>
+    <section class="back-slide" data-scroll data-scroll-speed="1.2">
       <main>
         <swiper
           :loop="true"
@@ -12,17 +12,21 @@
             <img :src="require(`./assets/images/banner0${i +1}.jpg`)">
           </swiper-slide>
         </swiper>
+        <div class="scroll-down">
+          <p></p>
+          <div>SCROLL</div>
+        </div>
       </main>
     </section>
 
     <section class="section" id="sectionPin">
       <div class="pin-wrap">
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <img src="./assets/images/banner01.jpg" alt="">
         <img src="./assets/images/banner02.jpg" alt="">
-        <img src="./assets/images/banner03.jpg" alt="">
         <img src="./assets/images/banner01.jpg" alt="">
-        <img src="./assets/images/banner02.jpg" alt="">
+        <img src="./assets/images/banner04.jpg" alt="">
+        <img src="./assets/images/banner05.jpg" alt="">
+        <img src="./assets/images/banner06.jpg" alt="">
         <img src="./assets/images/banner03.jpg" alt="">
         <img src="./assets/images/banner01.jpg" alt="">
         <img src="./assets/images/banner02.jpg" alt="">
@@ -30,15 +34,7 @@
       </div>
     </section>
 
-    <section>
-      <h2 data-scroll data-scroll-speed="1">Bye</h2>
-    </section>
-
   </div>
-  <!-- <div class="scroll-down">
-    <p></p>
-    <div>SCROLL</div>
-  </div> -->
 </template>
 
 <script>
@@ -47,9 +43,7 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-import LocomotiveScroll from 'locomotive-scroll'
-
-gsap.registerPlugin(ScrollTrigger);
+import LocomotiveScroll from 'locomotive-scroll';
 
 import 'swiper/css';
 
@@ -77,62 +71,61 @@ export default {
       onSlideChange,
     };
   },
-  mounted(){
-    const pageContainer = document.querySelector('.wrap')
-
-    const scroller = new LocomotiveScroll({
-      el: pageContainer,
-      smooth: true
-    });
-
-    scroller.on("scroll", ScrollTrigger.update);
-
-    ScrollTrigger.scrollerProxy(pageContainer, {
-      scrollTop(value){
-        return arguments.length
-          ? scroller.scrollTo(value, 0, 0)
-          : scroller.scroll.instance.scroll.y
-      },
-      getBoundingClientRect(){
-        return {
-          left: 0,
-          top: 0,
-          width: window.innerWidth,
-          height: window.innerHeight
-        }
-      },
-      pinType: document.querySelector('.wrap').style.transform ? "transform" : "fixed"
-    })
-
-    window.addEventListener('load', function (){
-      // let pinBoxes = document.querySelectorAll('.pin-wrap > *')
-      let pinWrap = document.querySelector('.pin-wrap')
-      let pinWrapWidth = pinWrap.offsetWidth
-      let horizontalScrollLength = pinWrapWidth - window.innerWidth
-
-      gsap.to(".pin-wrap", {
-        scrollTrigger: {
-          scroller: '.wrap', //locomotive-scroll
-          scrub: true,
-          trigger: "#sectionPin",
-          pin: true,
-          // anticipatePin: 1,
-          start: "top top",
-          end: pinWrapWidth
-        },
-        x: -horizontalScrollLength,
-        ease: "none"
-      });
-
-      ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-
-      ScrollTrigger.refresh();
-    })
-  },
   methods: {
-    scrollGsap(){
-      
+    setScroll(){
+      const scroller = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+      });
+      gsap.registerPlugin(ScrollTrigger);
+  
+      scroller.on("scroll", ScrollTrigger.update);
+  
+      ScrollTrigger.scrollerProxy('.wrap', {
+        scrollTop(value){
+          return arguments.length
+            ? scroller.scrollTo(value, 0, 0)
+            : scroller.scroll.instance.scroll.y
+        },
+        getBoundingClientRect(){
+          return {
+            left: 0,
+            top: 0,
+            width: window.innerWidth,
+            height: window.innerHeight
+          }
+        },
+        pinType: document.querySelector('.wrap').style.transform ? "transform" : "fixed"
+      })
+  
+      window.addEventListener('load', function (){
+        // let pinBoxes = document.querySelectorAll('.pin-wrap > *')
+        let pinWrap = document.querySelector('.pin-wrap')
+        let pinWrapWidth = pinWrap.offsetWidth
+        let horizontalScrollLength = pinWrapWidth - window.innerWidth
+  
+        gsap.to(".pin-wrap", {
+          scrollTrigger: {
+            scroller: '.wrap', //locomotive-scroll
+            scrub: true,
+            trigger: "#sectionPin",
+            pin: true,
+            anticipatePin: 1,
+            start: "top top",
+            end: pinWrapWidth
+          },
+          x: -horizontalScrollLength,
+          ease: "none"
+        });
+  
+        ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+  
+        ScrollTrigger.refresh();
+      })
     }
+  },
+  mounted(){
+    this.setScroll()
   }
 };
 </script>
