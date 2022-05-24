a90e85cc254ab6eae4ef925bfad9f13433362d40
10dy10 <kdy93102@naver.com>
2022-05-24 15:33:16 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,68 @@
   <Header />
   <div class="wrap">
     <section class="back-slide">
-      <main>        
+      <main>
         <swiper
-          :modules="modules"
-          :space-between="50"
-          navigation
-          :pagination="{ clickable: true }"
-          :scrollbar="{ draggable: true }"
+          :loop="true"
           @swiper="onSwiper"
           @slideChange="onSlideChange"
         >
-          <swiper-slide><img src="./assets/images/banner02.jpg" alt=""></swiper-slide>
-          <swiper-slide><img src="./assets/images/banner01.jpg" alt=""></swiper-slide>
-          <swiper-slide><img src="./assets/images/banner03.jpg" alt=""></swiper-slide>
-          <swiper-slide><img src="./assets/images/banner04.jpg" alt=""></swiper-slide>
-          <swiper-slide><img src="./assets/images/banner05.jpg" alt=""></swiper-slide>
-          <swiper-slide><img src="./assets/images/banner06.jpg" alt=""></swiper-slide>
+          <swiper-slide v-for="(a, i) in 6" :key="i">
+            <img :src="require(`./assets/images/banner0${i +1}.jpg`)">
+          </swiper-slide>
         </swiper>
       </main>
     </section>
+
+    <section class="section" id="sectionPin">
+      <div class="pin-wrap">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <img src="./assets/images/banner01.jpg" alt="">
+        <img src="./assets/images/banner02.jpg" alt="">
+        <img src="./assets/images/banner03.jpg" alt="">
+        <img src="./assets/images/banner01.jpg" alt="">
+        <img src="./assets/images/banner02.jpg" alt="">
+        <img src="./assets/images/banner03.jpg" alt="">
+        <img src="./assets/images/banner01.jpg" alt="">
+        <img src="./assets/images/banner02.jpg" alt="">
+        <img src="./assets/images/banner03.jpg" alt="">
+      </div>
+    </section>
+
+    <section>
+      <h2 data-scroll data-scroll-speed="1">Bye</h2>
+    </section>
+
   </div>
-  <div class="scroll-down">
+  <!-- <div class="scroll-down">
     <p></p>
     <div>SCROLL</div>
-  </div>
+  </div> -->
 </template>
 
 <script>
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import Header from "./layouts/TheHeader.vue";
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import Header from './layouts/TheHeader.vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+import LocomotiveScroll from 'locomotive-scroll'
+
+gsap.registerPlugin(ScrollTrigger);
 
 import 'swiper/css';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Swiper,
-    SwiperSlide,
+    SwiperSlide
   },
-  data(){
+  data() {
     return {
-      swiper: null,
-    }
+      
+    };
   },
   setup() {
     const onSwiper = (swiper) => {
@@ -57,10 +75,66 @@ export default {
     return {
       onSwiper,
       onSlideChange,
-      modules: [Navigation, Pagination, Scrollbar, A11y],
     };
   },
-}
+  mounted(){
+    const pageContainer = document.querySelector('.wrap')
+
+    const scroller = new LocomotiveScroll({
+      el: pageContainer,
+      smooth: true
+    });
+
+    scroller.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(pageContainer, {
+      scrollTop(value){
+        return arguments.length
+          ? scroller.scrollTo(value, 0, 0)
+          : scroller.scroll.instance.scroll.y
+      },
+      getBoundingClientRect(){
+        return {
+          left: 0,
+          top: 0,
+          width: window.innerWidth,
+          height: window.innerHeight
+        }
+      },
+      pinType: document.querySelector('.wrap').style.transform ? "transform" : "fixed"
+    })
+
+    window.addEventListener('load', function (){
+      // let pinBoxes = document.querySelectorAll('.pin-wrap > *')
+      let pinWrap = document.querySelector('.pin-wrap')
+      let pinWrapWidth = pinWrap.offsetWidth
+      let horizontalScrollLength = pinWrapWidth - window.innerWidth
+
+      gsap.to(".pin-wrap", {
+        scrollTrigger: {
+          scroller: '.wrap', //locomotive-scroll
+          scrub: true,
+          trigger: "#sectionPin",
+          pin: true,
+          // anticipatePin: 1,
+          start: "top top",
+          end: pinWrapWidth
+        },
+        x: -horizontalScrollLength,
+        ease: "none"
+      });
+
+      ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+      ScrollTrigger.refresh();
+    })
+  },
+  methods: {
+    scrollGsap(){
+      
+    }
+  }
+};
 </script>
 
 <style>
--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,10 +1,9 @@
 <template>
   <nav>
-      <router-link to="/"><img src="../assets/images/logo.png" alt=""></router-link>
     <ul>
       <li><router-link to="/intro">COMPANY</router-link></li>
       <li><router-link to="/artist">ARTIST</router-link></li>
-      <li><router-link to="/multimedia">MULTIMEDIA</router-link></li>
+      <router-link to="/"><img src="../assets/images/logo.png" alt=""></router-link>
       <li><a href="http://www.cubeent.co.kr/aud_index" target="_blank">AUDITION</a></li>
       <li><router-link to="/contact">CONTACT</router-link></li>
     </ul>
