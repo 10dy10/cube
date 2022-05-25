6461915aaf77e037186eff860bbcdf59ab3300aa
10dy10 <kdy93102@naver.com>
2022-05-25 16:46:03 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <Header />
-  <div class="wrap" data-scroll-container>
+  <div class="wrap">
     <section class="back-slide" data-scroll data-scroll-speed="1.2">
       <main>
         <swiper
+          :modules="modules"
+          :autoplay="{
+            delay: 5000,
+            disableOnInteraction: false,
+          }"
           :loop="true"
+          effect="fade"
           @swiper="onSwiper"
           @slideChange="onSlideChange"
         >
@@ -19,19 +25,21 @@
       </main>
     </section>
 
-    <section class="section" id="sectionPin">
+    <section id="sectionPin">
       <div class="pin-wrap">
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <img src="./assets/images/banner02.jpg" alt="">
-        <img src="./assets/images/banner01.jpg" alt="">
-        <img src="./assets/images/banner04.jpg" alt="">
-        <img src="./assets/images/banner05.jpg" alt="">
-        <img src="./assets/images/banner06.jpg" alt="">
-        <img src="./assets/images/banner03.jpg" alt="">
-        <img src="./assets/images/banner01.jpg" alt="">
-        <img src="./assets/images/banner02.jpg" alt="">
-        <img src="./assets/images/banner03.jpg" alt="">
+        <img src="./assets/images/banner02.jpg">
+        <img src="./assets/images/banner01.jpg">
+        <img src="./assets/images/banner04.jpg">
+        <img src="./assets/images/banner05.jpg">
+        <img src="./assets/images/banner06.jpg">
+        <img src="./assets/images/banner03.jpg">
+        <img src="./assets/images/banner07.jpg">
       </div>
+    </section>
+
+    <section class="bottom">
+      <Footer />
     </section>
 
   </div>
@@ -39,18 +47,24 @@
 
 <script>
 import Header from "./layouts/TheHeader.vue";
+import Footer from './layouts/TheFooter.vue'
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay, EffectFade } from 'swiper'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger);
 
 import LocomotiveScroll from 'locomotive-scroll';
 
 import 'swiper/css';
+import 'swiper/css/effect-fade';
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
     Swiper,
     SwiperSlide
   },
@@ -69,15 +83,15 @@ export default {
     return {
       onSwiper,
       onSlideChange,
+      modules: [ Autoplay, EffectFade ]
     };
   },
   methods: {
     setScroll(){
       const scroller = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
+        el: document.querySelector('.wrap'),
+        // smooth: true
       });
-      gsap.registerPlugin(ScrollTrigger);
   
       scroller.on("scroll", ScrollTrigger.update);
   
@@ -98,30 +112,29 @@ export default {
         pinType: document.querySelector('.wrap').style.transform ? "transform" : "fixed"
       })
   
-      window.addEventListener('load', function (){
-        // let pinBoxes = document.querySelectorAll('.pin-wrap > *')
-        let pinWrap = document.querySelector('.pin-wrap')
-        let pinWrapWidth = pinWrap.offsetWidth
-        let horizontalScrollLength = pinWrapWidth - window.innerWidth
-  
-        gsap.to(".pin-wrap", {
-          scrollTrigger: {
-            scroller: '.wrap', //locomotive-scroll
-            scrub: true,
-            trigger: "#sectionPin",
-            pin: true,
-            anticipatePin: 1,
-            start: "top top",
-            end: pinWrapWidth
-          },
-          x: -horizontalScrollLength,
-          ease: "none"
-        });
-  
-        ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-  
-        ScrollTrigger.refresh();
-      })
+      // let pinBoxes = document.querySelectorAll('.pin-wrap > *')
+      let pinWrap = document.querySelector('.pin-wrap')
+      let pinWrapWidth = pinWrap.offsetWidth
+      let horizontalScrollLength = pinWrapWidth - window.innerWidth
+
+      gsap.to(".pin-wrap", {
+        scrollTrigger: {
+          scroller: '.wrap', //locomotive-scroll
+          scrub: true,
+          trigger: "#sectionPin",
+          pin: true,
+          // anticipatePin: 1,
+          start: "top top",
+          end: pinWrapWidth,
+          markers: true,
+        },
+        x: -horizontalScrollLength,
+        ease: "none"
+      });
+
+      ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+      ScrollTrigger.refresh();
     }
   },
   mounted(){
@@ -133,5 +146,10 @@ export default {
 <style>
 @import url(./assets/css/reset.css);
 @import url(./assets/css/common.css);
+
+#app {
+  width: 100%;
+  height: 100%;
+}
 
 </style>
---
+++ src/layouts/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="left-logo">
+      <img src="../assets/images/logo.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+
+}
+</script>
+
+<style>
+
+</style>
